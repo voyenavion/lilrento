aea67084c5cda9c0b3249c9b3b9b1b9a30993ecc
Shaun Gould <shaun.gould@brivo.com>
2026-06-02 21:43:26 -0400

    preview test

--- src/constants/postConstants.ts
+++ src/constants/postConstants.ts
@@ -1,14 +1,14 @@
 export default [
   {
     title:
-      'Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label',
+      'title:Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label',
     creator: 'Upperfields',
     url: 'https://lilrento.com/upperfields/strange-grace',
     image: '/000526560002_b.jpg',
     description:
-      'Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label',
+      'description:Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label',
     siteName:
-      'Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label'
+      'siteName:Strange Grace - A Song By Upperfields - On Lil Rento - An Independent Record Label'
   }
 ]
 
